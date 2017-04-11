95ada9728cc0b4bbfa1de03378be1e482ca904c1
Shenghao Li <lishmac@gmail.com>
2017-04-10 18:59:10 -0700

    added material design login window
    Signed-off-by: Shenghao Li <lishmac@gmail.com>

--- app/src/main/java/com/example/shenghaoli/eventreporter/MainActivity.java
+++ app/src/main/java/com/example/shenghaoli/eventreporter/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shenghaoli.eventreporter;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.*;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello, World2!");
     }
+
 }
